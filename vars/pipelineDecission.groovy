3f55af12ba23cf858f23a40f42597c12c5d3a1fb
theppasiva <theppasiva@gmail.com>
2024-12-07 23:38:33 +0530

    updated pipelineDecission.groovy

--- vars/pipelineDecission.groovy
+++ vars/pipelineDecission.groovy
@@ -2,18 +2,18 @@
 
 def decidePipeline(Map configMap){
     application = configMap.get("application")
-switch(application) {
-  case 'nodejsVM':
-    nodejsVM(configMap)
-    break
-  case 'javaVM':
-    nodejsVM(configMap)
-    break
-  case 'nodejsEKS':
-    nodejsVM(configMap)
-    break
-  default:
-    error "Application is not recognised"
-    break
-}
+    switch(application) {
+        case 'nodejsVM':
+            nodejsVM(configMap)
+            break
+        case 'javaVM':
+            javaVM(configMap)
+            break
+        case 'nodejsEKS':
+            nodejsEKS(configMap)
+            break
+        default:
+            error "Application is not recognised"
+            break
+    }
 }
