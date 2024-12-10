ddf3794633b4928cf24fd960e32850b446ed8717
theppasiva <theppasiva@gmail.com>
2024-12-11 00:34:35 +0530

    updated nodejsvm.groovy

--- vars/nodejsVM.groovy
+++ vars/nodejsVM.groovy
@@ -97,7 +97,7 @@ pipeline {
                         def params = [
                             string(name: 'version', value: "$packageVersion"),
                             string(name: 'environment', value: "dev")
-                            booleanParam(name: 'Create', value: "${params.Deploy}")
+                            // booleanParam(name: 'Create', value: "${params.Deploy}")
                         ]
                         build job: "../${configMap.component}-deploy", wait: true, parameters: params
                     }
